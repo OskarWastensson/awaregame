44bc7d84c5de78741057d5213726b353b62bf1c7
Oskar Wastensson <oskar@wastensson.se>
2012-09-12 13:23:05 +0200

    Solution to asynchronizity issue

--- front/js/libs/backbone/backbone.js
+++ front/js/libs/backbone/backbone.js
@@ -1,9 +1,9 @@
 define([
 	'jQuery',
 	'Underscore',
-	'order!libs/backbone/backbone-0.9.2'
-	], function($, _){
-	
+	'libs/backbone/backbone-0.9.2'
+	], function(){
+	console.log($);
   // Now that all the orignal source codes have ran and accessed each other
   // We can call noConflict() to remove them from the global name space
   // Require.js will keep a reference to them so we can use them in our modules
--- front/js/main.js
+++ front/js/main.js
@@ -1,7 +1,3 @@
-// make FB a global object
-
-
-
 // Require.js allows us to configure shortcut alias
 require.config({
   paths: {
@@ -9,12 +5,8 @@ require.config({
     Underscore: 'libs/underscore/underscore',
     Backbone: 'libs/backbone/backbone'
   },
-  // urlArgs: "bust19",
-  urlArgs: "bust=" + (new Date()).getTime(),
-  Backbone: 'libs/backbone/backbone',
-	Parameters: 'libs/queryparams/backbone.queryparams'
-	// Facebook: 'libs/facebook/facebook'
-
+  // @TODO: Remove this on production server:
+  urlArgs: "bust=" + (new Date()).getTime()
 });
 
 require([
@@ -26,8 +18,8 @@ require([
   // Because these scripts are not "modules" they do not pass any values to the definition function below
   'order!libs/jquery/jquery-1.8.0',
   'order!libs/underscore/underscore-1.3.3',
-  'order!libs/backbone/backbone-0.9.2',
-  'order!libs/queryparams/backbone.queryparams'
+  'order!libs/backbone/backbone-0.9.2'
+  //'order!libs/queryparams/backbone.queryparams'
 ], function(App){
   // The "app" dependency is passed in as "App"
   // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
