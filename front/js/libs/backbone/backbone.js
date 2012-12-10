a01626b989732cf13da6378abd2474e3f2c5c568
Klara <kn1983@gmail.com>
2012-12-10 14:55:05 +0100

    changed the version for require.js

--- front/js/libs/backbone/backbone.js
+++ front/js/libs/backbone/backbone.js
@@ -4,16 +4,14 @@ define([
   // Now that all the orignal source codes have ran and accessed each other
   // We can call noConflict() to remove them from the global name space
   // Require.js will keep a reference to them so we can use them in our modules
-  
-	// Order! doens't work in Chrome, this is the ugly version.
-	var tester = setInterval(function() {
-		if(typeof _ != 'undefined' || typeof $ != 'undefined' || typeof jqueryui != 'undefined') {
-			_.noConflict();
-			$.noConflict();
-			Backbone.noConflict();
-			clearInterval(tester);
-		}
-	}, 30);
+	
+
+
+
+
+
+
+
 	
   return Backbone;
 });
