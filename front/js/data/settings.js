4f3f7c3b726f8b2d3ece5510536693cba74d79da
Oskar Wastensson <oskar@wastensson.se>
2012-12-10 16:24:10 +0100

    nothing

--- front/js/data/settings.js
+++ front/js/data/settings.js
@@ -4,17 +4,17 @@ define([
   	'Backbone'
 ], function($, _, Backbone){
 	var Settings = {
-			"name": "Mat",
-			"machineName": "food",
-			"facebookAppId": 416322788424429,
-			"restAPI": "localhost:8080/",
+			"name": "Mat", 
+			"machineName": "food", 
+			"facebookAppId": 416322788424429, 
+			"restAPI": "localhost:8080/", 
 			"welcomeText":
 				"Välkommen till Matspelet. Svara på några enkla frågor om " +
 				"dina matvanor och få reda på din miljöbelastning.",
 			"levels": [
 				{
-					"value": 0,
-					"title": "Du är en miljöbov!",
+					"value": 0, 
+					"title": "Du är en miljöbov!", 
 					"text": "Dina matvanor är rent förskräckliga. Medan du sitter till" +
 						" bords smälter glaciärerna. Men det är aldrig försent att bättra " +
 						"sig. Du kanske ska prova köttfri måndag?"
